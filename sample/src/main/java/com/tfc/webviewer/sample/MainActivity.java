3f4c6f7921b81d61c965f377c5202a46da5bcc11
Fobid <fobidbumz@naver.com>
2016-05-01 03:41:28 +0900

    add crashlytics

--- sample/src/main/java/com/tfc/webviewer/sample/MainActivity.java
+++ sample/src/main/java/com/tfc/webviewer/sample/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.tfc.webviewer.ui.WebViewerActivity;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         Intent intent = new Intent(this, WebViewerActivity.class);
