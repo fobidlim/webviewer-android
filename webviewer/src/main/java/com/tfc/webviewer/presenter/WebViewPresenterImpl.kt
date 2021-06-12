8145f90d17d2185234661751da01431b5c6f301b
fobidlim <fobidlim@gmail.com>
2021-06-12 18:01:15 +0900

    Update copyright

--- webviewer/src/main/java/com/tfc/webviewer/presenter/WebViewPresenterImpl.kt
+++ webviewer/src/main/java/com/tfc/webviewer/presenter/WebViewPresenterImpl.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright Fobid. All Rights Reserved.
+ * Copyright fobidlim. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
